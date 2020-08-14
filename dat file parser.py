caaf1b2fa5055dbafe26f6b63f67f3d114c8ff74
terpyPy <66324329+terpyPy@users.noreply.github.com>
2020-08-14 17:10:35 -0400

    Update dat file parser.py

--- dat file parser.py
+++ dat file parser.py
@@ -1,13 +1,15 @@
 import re
 import pprint
-
+import PySimpleGUI as sg
 CFFormat = ['CF ##']
 zeroSevenFormat = ['070707']
 fourTwoFourEightFormat = ['4248']
-formatsArr = [CFFormat, fourTwoFourEightFormat, zeroSevenFormat]
+nineEightZeroZeroFormat = ['9800 ##']
+formatsArr = [CFFormat, fourTwoFourEightFormat, zeroSevenFormat, nineEightZeroZeroFormat]
 formats = ['CF 00', 'CF 08', 'CF 07', 'CF 0F', 'CF F0', 'CF 09', 'CF 04',
            '42 48',
-           '07 07 07']
+           '07 07 07',
+           '98 00']
 hexLine = ''
 twoHexLines = ''
 matchDict = {}
@@ -15,10 +17,14 @@ offsetRE = re.compile(r'.*([\da-fA-F]{8})'
                       r'.*([\da-fA-F]{2} [\da-fA-F]{2} [\da-fA-F]{2} [\da-fA-F]{2} [\da-fA-F]{2} [\da-fA-F]{2} '
                       r'[\da-fA-F]{2} [\da-fA-F]{2} [\da-fA-F]{2} [\da-fA-F]{2} [\da-fA-F]{2} [\da-fA-F]{2} '
                       r'[\da-fA-F]{2} [\da-fA-F]{2} [\da-fA-F]{2})')
+
+
 def match_check(format, line, formatType):
     if (format in line) and (offset not in matchDict):
         matchDict[offset] = line
         formatType.append(offset)
+
+
 def cycle(line):
     x = 0
     for i in range(len(formats)):
@@ -27,18 +33,33 @@ def cycle(line):
             x += 1
 
 
-with open(input('please give a file name ')) as TXT_file:
-    for line in TXT_file:
-        hexMatch = offsetRE.search(line)
-        if hexMatch is not None:
-            offset, hexLine = hexMatch.groups()
-        cycle(hexLine)
-        twoHexLines = hexLine + ' ' + twoHexLines
-        if len(twoHexLines) > 100:
-            twoHexLines = ''
-        cycle(twoHexLines)
-pprint.pprint(matchDict)
-pprint.pprint(zeroSevenFormat)
-pprint.pprint(fourTwoFourEightFormat)
-pprint.pprint(CFFormat)
-TXT_file.close()
+sg.theme('DarkAmber')  # Add a touch of color
+# All the stuff inside your window.
+layout = [[sg.Text('enter the dat text file to parse'), sg.InputText()],
+          [sg.Output(size=(88, 20))],
+          [sg.Button('parse file'), sg.Button('Cancel')]]
+
+# Create the Window
+window = sg.Window('dat file parser', layout)
+
+# Event Loop to process "events" and get the "values" of the inputs
+while True:
+
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Cancel':  # if user closes window or clicks cancel
+        window.close()
+        break
+    if event == 'parse file' and values[0] is not None:
+        with open(values[0]) as TXT_file:
+            for line in TXT_file:
+                hexMatch = offsetRE.search(line)
+                if hexMatch is not None:
+                    offset, hexLine = hexMatch.groups()
+                cycle(hexLine)
+                twoHexLines = hexLine + ' ' + twoHexLines
+                if len(twoHexLines) > 100:
+                    twoHexLines = ''
+                cycle(twoHexLines)
+            pprint.pprint(matchDict)
+            pprint.pprint(formatsArr)
+            TXT_file.close()
